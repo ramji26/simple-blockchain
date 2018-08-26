1ee3e67b83edef41a5af09d2e103f6788293238c
ramji26 <ramji.er@gmail.com>
2018-08-26 18:34:29 +0530

    Added LevelDB.

--- simpleChain.js
+++ simpleChain.js
@@ -1,25 +1,35 @@
-const SHA256 = require('crytpo-js/sha256');
-
+const SHA256 = require('crypto-js/sha256');
+/**
+ * Class and methods for Blockchain
+ */
 class Block {
 
     constructor(data) {
         this.hash = "";
         this.height = 0;
         this.body = data;
-        this.time = new Date().getTime();
+        this.time = new Date().getTime().toString().slice(0, -3);
         this.previousblockhash = "";
     }
 
 }
-
+/**
+ * Class and methods for Blockchain
+ */
 class BlockChain {
 
     constructor() {
         this.chain = [];
+        this.addBlock(new Block("First Block in the chain - Genesis Block"));
     }
 
     addBlock(newBlock) {
 
+        if (this.chain.length > 0) {
+            newBlock.previousblockhash = this.chain[this.chain.length - 1].hash;
+            newBlock.height = this.chain.length;
+        }
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
         this.chain.push(newBlock);
     }
 
